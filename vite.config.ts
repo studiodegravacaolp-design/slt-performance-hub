aa67575f426a4e1cc700365d440cd1638c666f9e
Luís Paulo Assis Pereira <studiodegravacaolp@gmail.com>
2026-06-07 21:52:54 -0300

    Forçando preset Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 // @cloudflare/vite-plugin builds from this — wrangler.jsonc main alone is insufficient.
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
+    server: { 
+      entry: "server",
+      preset: "vercel"
+    },
   },
 });
 // Forçando build atualizado Vercel
